f427723643902ae3d436a0a751215098f38ef01a
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-04-25 14:53:24 +0700

    feat(view): creat label constant

---
+++ practice/src/scripts/constants/labels.ts
@@ -0,0 +1,12 @@
+export const LABELS = {
+  SKIN_CARE: 'Skin care',
+  FACE_CARE: 'Face care',
+  LIPS_CARE: 'Lip care',
+  BEST_SELLER: 'Best-seller',
+  POOR_SELLER: 'Poor seller',
+  ON_SALE: 'On sale',
+  NEW_ARRIVAL: 'New arrival',
+  LOW_STOCK: 'Low stock',
+  ALL_STATUS: 'All status',
+  ALL_CATEGORY: 'All category',
+} as const;
